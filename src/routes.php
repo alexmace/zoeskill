68637c5147accb0a12c28957bb3c6189a8322575
Alex Mace <alexmace@polaris-solutions.co.uk>
2018-01-04 22:43:40 +0000

    Switched the routes round so that the static route is interpreted first

--- src/routes.php
+++ src/routes.php
@@ -5,14 +5,6 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
-});
-
 // Flash Briefing Skill
 $app->get('/flash-briefing', function (Request $request, Response $response, array $args) {
     $this->logger->info("Zoe Skill '/flash-briefing' route");
@@ -23,6 +15,14 @@ $app->get('/flash-briefing', function (Request $request, Response $response, arr
         "titleText"     => "Amazon Developer Blog, week in review May 23rd",
         "mainText"      => "Meet Echosim. A new online community tool for developers that simulates the look and feel of an Amazon Echo.",
     ]);
+});
+
+$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
 });
 
 // Report:
