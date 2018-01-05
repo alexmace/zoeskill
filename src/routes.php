b97861308ccbee964cc60a569b392c4e1cf78e97
Alex Mace <alexmace@polaris-solutions.co.uk>
2018-01-05 22:10:51 +0000

    Replaced my entry with the example from Amazon's page

--- src/routes.php
+++ src/routes.php
@@ -14,10 +14,16 @@ $app->get('/flash-briefing', function (Request $request, Response $response, arr
               . 'a range of 200 miles. It is plugged in/not plugged in and is charging/not charging.';
 
     return $response->withJson([
-        "uid"           => "urn:uuid:" . Uuid::uuid4()->toString(),
-        "updateDate"    => (new DateTime())->format('Y-m-d\TH:i:s.0\Z'),
-        "titleText"     => "Current status of your Renault Zoe",
-        "mainText"      => $mainText,
+        "uid"               => "urn:uuid:1335c695-cfb8-4ebb-abbd-80da344efa6b",
+        "updateDate"        => "2016-05-23T00:00:00.0Z",
+        "titleText"         => "Amazon Developer Blog, week in review May 23rd",
+        "mainText"          => "Meet Echosim. A new online community tool for developers that simulates the look and feel of an Amazon Echo.",
+        "redirectionUrl"    => "https://developer.amazon.com/public/community/blog"
+
+        // "uid"           => "urn:uuid:" . Uuid::uuid4()->toString(),
+        // "updateDate"    => (new DateTime())->format('Y-m-d\TH:i:s.0\Z'),
+        // "titleText"     => "Current status of your Renault Zoe",
+        // "mainText"      => $mainText,
     ]);
 });
 
