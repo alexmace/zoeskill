70f7fa6e7ca05db77a24ad6ac8c561736131dba3
Alex Mace <alexmace@polaris-solutions.co.uk>
2018-01-05 22:48:56 +0000

    As the problem proved to be my letsencrypt cert, reverting to my previous feed

--- src/routes.php
+++ src/routes.php
@@ -14,17 +14,11 @@ $app->get('/flash-briefing', function (Request $request, Response $response, arr
               . 'a range of 200 miles. It is plugged in/not plugged in and is charging/not charging.';
 
     return $response->withJson([
-        "uid"               => "urn:uuid:1335c695-cfb8-4ebb-abbd-80da344efa6b",
-        "updateDate"        => "2016-05-23T00:00:00.0Z",
-        "titleText"         => "Amazon Developer Blog, week in review May 23rd",
-        "mainText"          => "Meet Echosim. A new online community tool for developers that simulates the look and feel of an Amazon Echo.",
-        "redirectionUrl"    => "https://developer.amazon.com/public/community/blog"
-
-        // "uid"           => "urn:uuid:" . Uuid::uuid4()->toString(),
-        // "updateDate"    => (new DateTime())->format('Y-m-d\TH:i:s.0\Z'),
-        // "titleText"     => "Current status of your Renault Zoe",
-        // "mainText"      => $mainText,
-    ])->withHeader('Content-Type', 'application/json');;
+        "uid"           => "urn:uuid:" . Uuid::uuid4()->toString(),
+        "updateDate"    => (new DateTime())->format('Y-m-d\TH:i:s.0\Z'),
+        "titleText"     => "Current status of your Renault Zoe",
+        "mainText"      => $mainText,
+    ]);
 });
 
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
