a3a2d5d14e4ecf3898d074df417b9d55929c5356
Alex Mace <alexmace@polaris-solutions.co.uk>
2018-01-18 21:24:13 +0000

    Added empty test cases for ZeServices and MyRenault classes

---
+++ tests/Renault/MyRenaultTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AlexMace\ZoeSkill\Renault;
+
+use PHPUnit\Framework\TestCase;
+
+class MyRenaultTest extends TestCase
+{
+    public function testDummy()
+    {
+        $this->markTestIncomplete();
+    }
+}
---
+++ tests/Renault/ZeServicesTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AlexMace\ZoeSkill\Renault;
+
+use PHPUnit\Framework\TestCase;
+
+class ZeServicesTest extends TestCase
+{
+    public function testDummy()
+    {
+        $this->markTestIncomplete();
+    }
+}
