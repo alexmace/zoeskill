30aff4d2cd1610a5ab1b4251d46315dff6f67982
Alex Mace <alexmace@polaris-solutions.co.uk>
2018-02-02 21:26:38 +0000

    Removed the fake information now that we can really make requests

--- src/dependencies.php
+++ src/dependencies.php
@@ -28,25 +28,7 @@ $container['logger'] = function ($c) {
 
 $container['zeservices'] = function ($c) {
     $settings = $c->get('settings')['zeservices'];
-
-    $container = [];
-    $history = Middleware::history($container);
-
-    // Create a mock and queue two responses.
-    $mockHandler = new MockHandler([]);
-
-    $stack = HandlerStack::create($mockHandler);
-    // Add the history middleware to the handler stack.
-    $stack->push($history);
-
-    // Maybe move this into setupResponse function?
-    $mockHandler->append(
-        new Response(200, [], json_encode(['token' => 'AAAA', 'vehicle_details' => ['VIN' => 'VVVV']])),
-        new Response(200, [], json_encode(["charging" => false, "plugged" => true, "charge_level" => 100, "remaining_range" => 124.0, "last_update" => 1476472742000, "charging_point" => "INVALID"]))
-    );
-
     $client = new Client([
-        'handler'   => $stack,
         'base_uri'  => $settings['baseUri'],
     ]);
     $ze = new ZeServices($client);
