3f62b3f4802de113cf8e9fc60830f042e6cf653d
Alex Mace <alexmace@polaris-solutions.co.uk>
2018-04-01 12:04:50 +0100

    Added authentication for the arrival/leaving paths

--- src/middleware.php
+++ src/middleware.php
@@ -5,3 +5,8 @@ $container = $app->getContainer();
 $settings = $container->get('settings');
 
 $app->add(new \AlexMace\ZoeSkill\Middleware\Alexa($settings['applicationId']));
+
+$app->add(new \Slim\Middleware\HttpBasicAuthentication([
+    'path' => $settings['path'],
+    'users' => $settings['users'],
+]));
